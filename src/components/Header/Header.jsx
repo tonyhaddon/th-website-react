10b49dfb895497fed60643610325a86d68a07f1e
tonyhaddon-bb <tony.haddon@britebill.com>
2018-12-04 10:45:14 +0000

    Last check on mobile

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,16 +5,16 @@ import styles from './Header.css';
 export class Header extends React.Component {
 	render() {
 		return (
-			<div>
-				<div className={(this.props.fadeIt) ? styles.headerNormal : styles.headerHidden}>
-					<p className={styles.headerTitle}>Tony Haddon</p>
-					<p className={styles.headerSubtitle}>Design CV</p>
-					<div className={styles.actionsPanel}>
-						<HeaderActionIcon actionHref="mailto:tony.haddon@gmail.com" actionType="contact" actionText='Contact'/>
-						<HeaderActionIcon actionHref="/docs/cv_tonyhaddon_2018.pdf" actionType="pdf" actionText='Download PDF version'/>
-					</div>
+			
+			<div className={(this.props.fadeIt) ? styles.headerNormal : styles.headerHidden}>
+				<p className={styles.headerTitle}>Tony Haddon</p>
+				<p className={styles.headerSubtitle}>Design CV</p>
+				<div className={styles.actionsPanel}>
+					<HeaderActionIcon actionHref="mailto:tony.haddon@gmail.com" actionType="contact" actionText='Contact'/>
+					<HeaderActionIcon actionHref="/docs/cv_tonyhaddon_2018.pdf" actionType="pdf" actionText='Download PDF version'/>
 				</div>
 			</div>
+			
 		);
 	};
 }
